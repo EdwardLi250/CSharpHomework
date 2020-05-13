36e87cd0cb05fb9794e861a60e2a16f670e85866
EdwardLi250 <45117035+EdwardLi250@users.noreply.github.com>
2020-05-13 23:54:09 +0800

    Add files via upload

---
+++ Homework12(OrderSystem_Api)/WebApplication1/Controllers/OrderController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        private readonly OrderContext orderContext;
+
+        public OrderController(OrderContext orderContext)
+        {
+            this.orderContext = orderContext;
+        }
+
+        private IQueryable<Order> AllOrders()
+        {
+            return orderContext.Orders.Include("Orderitems");
+        }
+
+        //GET: api/order
+        [HttpGet]
+        public ActionResult<List<Order>> GetAllOrders()
+        {
+            var orders = AllOrders();
+            return orders.ToList();
+        }
+
+        //GET: api/order/ini
+        [HttpGet("ini")]
+        public ActionResult<List<Order>> Initialize()
+        {
+            List<OrderItem> order1_items = new List<OrderItem>();
+            List<OrderItem> order2_items = new List<OrderItem>();
+            OrderItem order1_item1 = new OrderItem("Blonde", 3, 50);
+            OrderItem order1_item2 = new OrderItem("Orange", 2, 35);
+            OrderItem order2_item1 = new OrderItem("IGOR", 5, 50);
+            order1_items.Add(order1_item1); order1_items.Add(order1_item2);
+            order2_items.Add(order2_item1);
+            Order order1 = new Order(1, "FO", true, DateTime.Parse("2020/02/01 11:22:33"), order1_items);
+            Order order2 = new Order(2, "TC", true, DateTime.Parse("2020/02/01 11:22:33"), order2_items);
+
+            orderContext.Orders.Add(order1);
+            orderContext.Orders.Add(order2);
+            orderContext.SaveChanges();
+            //return Ok(AllOrders().ToList());
+            return GetAllOrders();
+        }
+
+        //POST: api/order
+        [HttpPost]
+        public ActionResult<Order> AddOrder(Order order)
+        {
+            try
+            {
+                orderContext.Orders.Add(order);
+                orderContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+            return order;
+        }
+
+        //GET: api/order/{id}
+        [HttpGet("{id}")]
+        public ActionResult<List<Order>> GetOrderByID(int id)
+        {
+            var query = orderContext.Orders.Include("Orderitems").Where(p => p.OrderID == id);
+            //var query = Orders.Where(o => o.OrderID == id);
+            return query.ToList();
+        }
+
+        //GET: api/order/time?=
+        [HttpGet("time")]
+        public ActionResult<List<Order>> GetOrderByTime(DateTime order_time)
+        {
+            
+            var query = orderContext.Orders.Include("Orderitems").Where(p => p.Time == order_time).OrderBy(o => o.TotalPrice);
+            //var query = Orders.Where(o => o.Time == order_time).OrderBy(o => o.TotalPrice);
+            return query.ToList();
+        }
+
+        //GET: api/order/customer?name=
+        [HttpGet("customer")]
+        public ActionResult<List<Order>> GetOrderByCustomer(string order_customer)
+        {
+            var query = orderContext.Orders.Include("Orderitems").Where(p => p.Customer == order_customer).OrderBy(o => o.TotalPrice);
+            //var query = Orders.Where(o => o.Customer == order_customer).OrderBy(o => o.TotalPrice);
+            return query.ToList();
+        }
+
+        //DELETE: api.order/{id}
+        [HttpDelete("{id}")]
+        public ActionResult<Order> DeleteOrder(int id)
+        {
+            try
+            {
+                //Orders.Remove(Orders.Where(p => p.OrderID == id).First());
+                var order = orderContext.Orders.Include("Orderitems").Where(p => p.OrderID == id).First();
+                orderContext.Orders.Remove(order);
+                //context.Orders.Remove(Orders.Where(p => p.OrderID == id).First());
+                orderContext.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+        }
+    }
+}
+
+
+
+
+
+    
---
+++ Homework12(OrderSystem_Api)/WebApplication1/Models/OrderSystem.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication1
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+            this.Database.EnsureCreated(); 
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+    }
+    public class Order
+    {
+
+        public Order() { }
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]//不自动增长
+        public int OrderID { get; set; }
+
+        public string Customer { get; set; }
+        private bool IsCompleted { get; set; }
+        public DateTime Time { get; set; }
+        public double TotalPrice { get; set; }
+        public List<OrderItem> Orderitems { get; set; }
+        public Order(int id, string customer, bool iscompleted,
+            DateTime time, List<OrderItem> items)
+        {
+            OrderID = id;
+            Customer = customer;
+            IsCompleted = iscompleted;
+            Time = time;
+            TotalPrice = 0;
+            foreach (OrderItem item in items)
+                TotalPrice += item.Price * item.Quantity;
+            Orderitems = items;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder all_items = new StringBuilder();
+            foreach (OrderItem oi in Orderitems)
+            {
+                all_items.Append(oi.ToString() + "\n");
+            }
+            return "ID: " + OrderID + "   " + "Created time: " + Time + "   " + "Customer's name: " + Customer
+                + "\n" + all_items.ToString() +
+                "Total price: " + TotalPrice + "\n" + "Completed: " + IsCompleted;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Order order &&
+                   OrderID == order.OrderID;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -236961719;
+            hashCode = hashCode * -1521134295 + OrderID.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Customer);
+            hashCode = hashCode * -1521134295 + IsCompleted.GetHashCode();
+            hashCode = hashCode * -1521134295 + Time.GetHashCode();
+            hashCode = hashCode * -1521134295 + TotalPrice.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<List<OrderItem>>.Default.GetHashCode(Orderitems);
+            return hashCode;
+        }
+    }
+
+    public class OrderItem
+    {
+        public OrderItem() { }
+        [Key]
+        public int ItemNum { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public int OrderID { get; set; }
+        public virtual Order Order { get; set; }
+        public OrderItem(string name, int number, double price)
+        {
+            Name = name;
+            Quantity = number;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return "Product name: " + Name + "   " + "Quantity: " + Quantity + "   Price: " + Price;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is OrderItem item &&
+                   Name == item.Name &&
+                   Quantity == item.Quantity &&
+                   Price == item.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1978195187;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + Quantity.GetHashCode();
+            hashCode = hashCode * -1521134295 + Price.GetHashCode();
+            return hashCode;
+        }
+    }
+
+}
